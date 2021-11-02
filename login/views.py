f66ef9a10cf87c13878295c56576463ac768e5f5
warhunt <pochtarossii123456789@gmail.com>
2021-11-02 08:36:46 +0300

    add redirect for auth user

--- login/views.py
+++ login/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from main.models import Message
 from django.conf import settings 
 
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('main')
     number_message = [len(Message.objects.filter(body=button)) for button in settings.CONST.get('buttons')]
     return render(request, 'login.html', {'buttons': settings.CONST.get('buttons'), 'len': number_message})
